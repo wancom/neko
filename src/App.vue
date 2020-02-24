83ca90c97a240cd14adc7c2598a03f502d675618
wancom <softpiware@gmail.com>
2020-02-24 20:33:48 +0900

    Cat walks randomly

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Neko
+      ref="neko"
+      @mouseenter="
+        () => {
+          randomWalk(setRandomWalk);
+        }
+      "
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Neko from "./components/Neko.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Neko
+  },
+  data() {
+    return {
+      randtimer: null
+    };
+  },
+  mounted() {
+    this.setRandomWalk();
+  },
+  methods: {
+    randomWalk(callback) {
+      clearTimeout(this.randtimer);
+
+      const x = Math.floor(Math.random() * window.innerWidth);
+      const y = Math.floor(Math.random() * window.innerHeight);
+      this.$refs.neko.walkToTarget(x, y, callback);
+    },
+    setRandomWalk() {
+      clearTimeout(this.randtimer);
+      const self = this;
+      const ms = Math.floor(Math.random() * 5) * 1000;
+      console.log(ms);
+      if (ms == 0) return;
+      this.randtimer = setTimeout(() => {
+        this.randomWalk(self.setRandomWalk);
+      }, ms);
+    }
   }
 };
 </script>
@@ -23,6 +56,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+body {
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+html {
+  width: 100%;
+  height: 100%;
 }
 </style>
---
+++ src/components/Neko.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="neko">
+    <div v-if="status == 0" @mouseenter="$emit('mouseenter')">
+      <img
+        alt="catsit"
+        src="../assets/sit.png"
+        :style="{ left: x - 50 + 'px', top: y - 50 + 15 + 'px' }"
+      />
+    </div>
+
+    <div v-if="status == 1" @mouseenter="$emit('mouseenter')">
+      <img
+        alt="catwalk2"
+        src="../assets/walk3.png"
+        :style="{
+          left: x - 50 + 'px',
+          top: y - 50 + 'px'
+        }"
+      />
+    </div>
+    <div v-if="status == 2">
+      <img
+        :alt="'catwalk' + String(walking)"
+        :src="require('../assets/walk' + String(walking) + '.png')"
+        :style="{
+          left: x - 50 + 'px',
+          top: y - 50 + 'px',
+          transform:
+            'rotate(' +
+            getAngle() +
+            'deg) rotateY(' +
+            (targetx - x < 0 ? 0 : 180) +
+            'deg)'
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// 0: sit
+// 1: stand
+// 2: walk
+export default {
+  name: "Neko",
+  props: {
+    ix: Number,
+    iy: Number
+  },
+  data() {
+    return {
+      walking: 1,
+      status: 0,
+      x: this.ix | (window.innerWidth / 2),
+      y: this.ix | (window.innerHeight / 2),
+      targetx: this.ix | (window.innerWidth / 2),
+      targety: this.ix | (window.innerHeight / 2),
+      walktimer: null,
+      sittimer: null,
+      walkcount: 0
+    };
+  },
+  mounted() {
+    // setTimeout(() => {
+    // this.x = 0;
+    // this.y = 0;
+    // this.targetx = 100;
+    // this.targety = 100;
+    document
+      .getElementsByTagName("body")[0]
+      .addEventListener("mousedown", e => {
+        this.walkToTarget(e.clientX, e.clientY);
+      });
+    //         this.walk();
+
+    // }, 3000);
+  },
+  methods: {
+    walk(callback) {
+      if (this.status == 2) {
+        return;
+      }
+      clearInterval(this.sittimer);
+      const self = this;
+      this.status = 2;
+
+      this.walktimer = setInterval(() => {
+        if (
+          Math.abs(self.targetx - self.x) < 3 &&
+          Math.abs(self.targety - self.y) < 3
+        ) {
+          clearInterval(self.walktimer);
+          // self.sit();
+
+          self.status = 1;
+          self.sittimer = setTimeout(() => {
+            self.sit();
+          }, 5000);
+          if (callback) callback();
+        }
+        const dx = self.targetx - self.x;
+        const dy = self.targety - self.y;
+        const rad = Math.atan(dy / dx);
+        const r = 3;
+        if (dx != 0) self.x += Math.cos(rad) * r * (dx / Math.abs(dx));
+        if (dy != 0)
+          self.y += Math.sin(Math.abs(rad)) * r * (dy / Math.abs(dy));
+        // console.log(dx, dy, rad, Math.cos(rad), self.x, self.y);
+        // console.log(dy / Math.abs(dy));
+        // self.xx = 0;
+        self.walkcount += 1;
+        if (self.walkcount == 5) {
+          self.walkcount = 0;
+
+          self.walking += 1;
+          if (self.walking == 9) {
+            self.walking = 1;
+          }
+        }
+        // if (self.x < -100) self.x = window.innerWidth;
+      }, 50);
+    },
+    walkToTarget(x, y, callback) {
+      this.targetx = x;
+      this.targety = y;
+      this.walk(callback);
+    },
+    sit() {
+      if (this.status == 2) {
+        clearInterval(this.walktimer);
+      }
+      this.status = 0;
+    },
+    getAngle() {
+      const dx = this.targetx - this.x;
+      const dy = this.targety - this.y;
+      const rad = Math.atan(dy / dx);
+      const deg = (rad * 180) / Math.PI;
+      // console.log(dx,dy,rad,deg)
+
+      if (deg < 90) return deg;
+      else return 180 - deg;
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  /* top:320px;
+  left:200px; */
+}
+</style>
